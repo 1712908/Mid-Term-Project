c7707c37e07bc5bb33f7a7251b71bb78d4f0839e
Dang Xuan Vinh <vinhdang781@gmail.com>
2018-04-10 12:25:40 +0700

    Doc file thanh cong

--- 1712908/1712908/Source.cpp
+++ 1712908/1712908/Source.cpp
@@ -5,38 +5,75 @@
 #include<wchar.h>
 
 struct SV{
-	wchar_t MSSV[20];
-	wchar_t HVTen[30];
-	wchar_t Khoa[30];
-	int KhoaHoc;
-	wchar_t NgaySinh[11];
-	wchar_t Hinhanh[16];
-	wchar_t Motabanthan[1001];
-	wchar_t Sothich1[101];
-	wchar_t Sothich2[101];
+	wchar_t* MSSV;
+	wchar_t* HVTen;
+	wchar_t* Khoa;
+	wchar_t* KhoaHoc;
+	wchar_t* NgaySinh;
+	wchar_t* Hinhanh;
+	wchar_t* Motabanthan;
+	wchar_t** Sothich;
 };
 
-void DocFILECSV(FILE* f, SV &A)
+void DocFILECSV(FILE* f, SV &A, int &demsothich)
 {
 	if (f != NULL)
 	{
-		fgetws(A.MSSV, 14, f);
-		A.MSSV[wcslen(A.MSSV) - 1] = L'\0';
+		demsothich = 0;
+		wchar_t* flag[20];
+		wchar_t* del;
+		wchar_t s[500];
+		fgetws(s, 500, f);
+		s[wcslen(s) - 1] = L'\0';
+		del = wcstok(s, L",\"");
+		A.MSSV = wcstok(NULL, L",\"");
+		A.HVTen = wcstok(NULL, L",\"");
+		A.Khoa = wcstok(NULL, L",\"");
+		A.KhoaHoc = wcstok(NULL, L",\"");
+		A.NgaySinh = wcstok(NULL, L",\"");
+		A.Hinhanh = wcstok(NULL, L",\"");
+		A.Motabanthan = wcstok(NULL, L",\"");
+		flag[demsothich] = wcstok(NULL, L",\"");
+		while (flag[demsothich] != NULL)
+		{
+			printf("%d\n", demsothich);
+			demsothich++;
+			flag[demsothich] = wcstok(NULL, L",\"");
+		}
+		printf("%d\n", demsothich);
+		A.Sothich = new wchar_t*[demsothich];
+		for (int i = 0; i < demsothich; i++)
+		{
+			*(A.Sothich + i) = flag[i];
+		}
 	}
-	fclose(f);
 }
 
 void main()
 {
+	int n;
 	FILE* f = fopen("1712908.csv", "rt");
-	SV A;
-	_setmode(_fileno(stdout), _O_U16TEXT); 
-	_setmode(_fileno(stdin), _O_U16TEXT);
-	DocFILECSV(f, A);
 	FILE* f1 = fopen("B.txt", "wt");
+	SV A;
+	_setmode(_fileno(f1), _O_U8TEXT); 
+	_setmode(_fileno(f), _O_U8TEXT);
+	//_setmode(_fileno(stdin), _O_U8TEXT);
+	//_setmode(_fileno(stdout), _O_U8TEXT);
+	DocFILECSV(f, A, n);
 	if (f1 != NULL)
 	{
 		fputws(A.MSSV, f1);
+		fputws(A.HVTen, f1);
+		fputws(A.Khoa, f1);
+		fputws(A.KhoaHoc, f1);
+		fputws(A.NgaySinh, f1);
+		fputws(A.Hinhanh, f1);
+		fputws(A.Motabanthan, f1);
+		for (int i = 0; i < n; i++)
+		{
+			fputws(*(A.Sothich + i), f1);
+		}
 	}
+	fclose(f);
 	fclose(f1);
 }
